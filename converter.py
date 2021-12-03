0bccddfccb86115c853462c3213878233089dfc1
Shubham <shubhamprakash702@gmail.com>
2021-12-03 20:07:36 +0530

    added the option to change the font size of ASCII image

--- converter.py
+++ converter.py
@@ -9,21 +9,20 @@ with open('data.json', 'r') as f:
 data = json.loads(json_string)
 
 scale_factor = 0.3
-one_character_height = 18
-one_character_width = 8
-char = '@#ADBI!i+*^"\' '[::-1]
 
-def get_resized_image(path):
-    img = cv2.imread(path, -1)
-    img = cv2.resize(img, (0,0), fx=scale_factor, fy=scale_factor*one_character_width/one_character_height)
-    return img
+char = '@#ADBI!i+*^"\' '[::-1]
+# char = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{/}[]?-_+~<>i!lI;:,"^`\'.'[::-1]
 
 def get_character(n):
     charlist = list(char)
     interval = len(charlist)/256
     return charlist[math.floor(n*interval)]
 
-def get_text_ASCII_art(img, file_name):
+def get_text_ASCII_art(path, file_name):
+    one_character_height = 18
+    one_character_width = 8
+    img = cv2.imread(path, -1)
+    img = cv2.resize(img, (0,0), fx=scale_factor, fy=scale_factor*one_character_width/one_character_height)
     gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     h, w = gray_image.shape
     with open(file_name, 'w') as f:
@@ -32,18 +31,25 @@ def get_text_ASCII_art(img, file_name):
                 f.write(get_character(gray_image[i][j]))
             f.write('\n')
 
-def get_jpg_ASCII_art(img):
+def get_jpg_ASCII_art(path):
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 0.4
+    font_thickness = 1
+    textsize = cv2.getTextSize('@', font, font_scale, font_thickness)[0]
+    char_width, char_height = textsize
+    char_height= char_height + 4
+    img = cv2.imread(path, -1)
+    img = cv2.resize(img, (0,0), fx=scale_factor, fy=scale_factor*char_width/char_height)
     gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     h, w = gray_image.shape
-    image = np.zeros((one_character_height*h,one_character_width*w,3), np.uint8)
-    font = cv2.FONT_HERSHEY_COMPLEX
+    image = np.zeros((char_height*h,char_width*w,3), np.uint8)
     for i in range(h):
         for j in range(w):
             b, g, r = img[i][j]
             b = int(b)
             g = int(g)
             r = int(r)
-            image = cv2.putText(image, get_character(gray_image[i][j]), (j*one_character_width,(i+1)*one_character_height), font, 0.45, (b,g,r), 1, cv2.LINE_AA)
+            image = cv2.putText(image, get_character(gray_image[i][j]), (j*char_width,(i+1)*char_height), font, font_scale, (b,g,r), font_thickness, cv2.LINE_AA)
     return image
 
 
@@ -56,9 +62,8 @@ if __name__ == "__main__":
             path = data['path_of_input_folder'] + name
             text_file_name = data['path_of_ASCII_art_text'] + image_name + '_ASCII_art.text'
             image_file_name = data['path_of_ASCII_art_image'] + image_name + '_ASCII_art.jpg'
-            img = get_resized_image(path)
-            get_text_ASCII_art(img, text_file_name)
-            image = get_jpg_ASCII_art(img)
+            get_text_ASCII_art(path, text_file_name)
+            image = get_jpg_ASCII_art(path)
             cv2.imwrite(image_file_name, image)
             print('Your image is converted and saved')
             break
